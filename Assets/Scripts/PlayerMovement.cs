61410e2afd5c3f023d296cca49f63bb86c6c67a0
nixon-voxell <voxell.technologies@gmail.com>
2024-09-28 03:02:27 +0800

    Add simple movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float m_Speed = 1.0f;
+
     private void Start()
     {
     }
 
     private void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(horizontal, vertical).normalized;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(horizontal, vertical, 0.0f).normalized;
+
+        this.transform.position += this.m_Speed * movement * Time.deltaTime;
 
         Debug.Log($"{movement}");
     }
