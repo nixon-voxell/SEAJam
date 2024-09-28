5273ad35d0db78e6312c5ad79d213c65a1f0a405
nixon-voxell <voxell.technologies@gmail.com>
2024-09-28 11:14:43 +0800

    Add stamina speed boost

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
+using Voxell.Util;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float m_Speed = 1.0f;
+    [Tooltip("Base walking speed of the player.")]
+    [SerializeField] private float m_Speed;
+    [Tooltip("Amount of additional speed added to the base speed when stamina is being used.")]
+    [SerializeField] private float m_BoostSpeed;
+    [SerializeField] private PlayerStamina m_Stamina;
+
+    private void Awake()
+    {
+        this.m_Stamina.Init();
+    }
 
     private void Update()
     {
@@ -10,7 +20,66 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 movement = new Vector3(horizontal, vertical, 0.0f).normalized;
 
+        bool useStamina = Input.GetButton("Jump");
+
+        float speed = this.m_Speed;
+        if (useStamina && this.m_Stamina.DepleteStamina())
+        {
+            speed += this.m_BoostSpeed;
+        }
+
         // Move the player based on input.
-        this.transform.position += this.m_Speed * movement * Time.deltaTime;
+        this.transform.position += movement * speed * Time.deltaTime;
+    }
+}
+
+[System.Serializable]
+public struct PlayerStamina
+{
+    [SerializeField] private float m_MaxStamina;
+
+    [Tooltip("Amount of stamina to deplete per second.")]
+    [SerializeField] private float m_DepletionFactor;
+
+    [SerializeField, InspectOnly] private float m_CurrStamina;
+
+    public void Init()
+    {
+        this.m_CurrStamina = this.m_MaxStamina;
+    }
+
+    public float GetMaxStamina()
+    {
+        return this.m_MaxStamina;
+    }
+
+    public float CurrStamina()
+    {
+        return this.m_CurrStamina;
+    }
+
+    /// <summary>
+    /// Checks if the player still has stamina left.
+    /// </summary>
+    public bool HasStamina()
+    {
+        return this.CurrStamina() > 0.0f;
+    }
+
+    /// <summary>
+    /// Depletes stamina and returns true if there is stamina left,
+    /// and false if there isn't any left.
+    /// </summary>
+    public bool DepleteStamina()
+    {
+        if (this.HasStamina())
+        {
+            this.m_CurrStamina -= this.m_DepletionFactor * Time.deltaTime;
+            // Clamp above 0.0f
+            this.m_CurrStamina = Mathf.Max(this.m_CurrStamina, 0.0f);
+            return true;
+        }
+
+        return false;
     }
 }
