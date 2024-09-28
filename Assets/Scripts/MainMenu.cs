0d73fa73f795d29c3198180cb4c8b758daafac91
nixon-voxell <voxell.technologies@gmail.com>
2024-09-29 02:25:40 +0800

    Implement play button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,7 +1,15 @@
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField, Voxell.Util.Scene] public string m_GameScene;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(this.m_GameScene, LoadSceneMode.Single);
+    }
+
     public void ExitGame()
     {
         Debug.Log("Quit Game");
