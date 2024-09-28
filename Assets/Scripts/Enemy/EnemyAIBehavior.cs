e56288144d651b5065cba9acbb0c74f23b2d875c
Miisan <azarmisan@gmail.com>
2024-09-29 03:30:26 +0800

    add: Enemy ai with animation!

--- Assets/Scripts/Enemy/EnemyAIBehavior.cs
+++ Assets/Scripts/Enemy/EnemyAIBehavior.cs
@@ -3,53 +3,74 @@ using System.Collections;
 
 public class EnemyPatrolAI : MonoBehaviour
 {
-    public float patrolDistance = 5f;
-    public float moveSpeed = 2f;
-    public float idleTime = 2f;
-    public bool isHorizontal = true;
+    public float patrolDistance = 5f, moveSpeed = 2f, idleTime = 2f;
+    public bool isHorizontal = true, startFacingRight = true;
+    public Animator animator;
+    public AnimationClip idleUpAnim, idleDownAnim, idleSideAnim, walkUpAnim, walkDownAnim, walkSideAnim;
 
-    private Vector3 startPosition;
-    private Vector3 endPosition;
+    private Vector3 startPosition, endPosition, movement;
     private bool movingToEnd = true;
 
-    private void Start()
+    void Start()
     {
         startPosition = transform.position;
         CalculateEndPosition();
         StartCoroutine(PatrolCoroutine());
+        animator ??= GetComponent<Animator>();
+        SetFacingDirection(startFacingRight);
     }
 
-    private void CalculateEndPosition()
-    {
-        if (isHorizontal)
-        {
-            endPosition = startPosition + Vector3.right * patrolDistance;
-        }
-        else
-        {
-            endPosition = startPosition + Vector3.up * patrolDistance;
-        }
-    }
+    void CalculateEndPosition() => endPosition = startPosition + (isHorizontal ? Vector3.right : Vector3.up) * patrolDistance;
 
-    private IEnumerator PatrolCoroutine()
+    IEnumerator PatrolCoroutine()
     {
         while (true)
         {
             Vector3 targetPosition = movingToEnd ? endPosition : startPosition;
+            movement = (targetPosition - transform.position).normalized;
 
-            while (transform.position != targetPosition)
+            while (Vector3.Distance(transform.position, targetPosition) > 0.05f)
             {
                 transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+                if (isHorizontal)
+                    SetFacingDirection(movement.x > 0 ? true : false);  
+
+                UpdateAnimation();
                 yield return null;
             }
 
+            movement = Vector3.zero;
+            UpdateAnimation();
             yield return new WaitForSeconds(idleTime);
             movingToEnd = !movingToEnd;
         }
     }
+
+    void UpdateAnimation()
+    {
+        if (movement.magnitude < 0.1f)
+            PlayAnimation(idleUpAnim);
+        else if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+            PlayAnimation(walkSideAnim);
+        else
+            PlayAnimation(movement.y > 0 ? walkUpAnim : walkDownAnim);
+    }
+
+    void PlayAnimation(AnimationClip clip) => animator.Play(clip.name);
+
+    void SetFacingDirection(bool faceLeft)
+    {
+        Vector3 newScale = transform.localScale;
+        newScale.x = Mathf.Abs(newScale.x) * (faceLeft ? -1 : 1);  
+        transform.localScale = newScale;
+    }
+
     public void ToggleDirection()
     {
         isHorizontal = !isHorizontal;
         CalculateEndPosition();
+        SetFacingDirection(!startFacingRight);
+        startFacingRight = !startFacingRight;
     }
 }
