d01dbc2138e7239db2a5b7ac5388ddacba1d2e0a
nixon-voxell <voxell.technologies@gmail.com>
2024-09-29 14:25:30 +0800

    Wrench & valve works

--- Assets/Scripts/Puzzles/PipeFix/KeyItemFix.cs
+++ Assets/Scripts/Puzzles/PipeFix/KeyItemFix.cs
@@ -21,7 +21,7 @@ public class KeyItemFix : Puzzle
     [SerializeField] SpriteRenderer _ProgressionMeter;
     [SerializeField] GameObject _Bar;
     [SerializeField] UnityEvent _FixedEvent;
-    public System.Action<float> _ProgressionAction;
+    public System.Action<float> ProgressionAction;
 
     #region Properties
 
@@ -83,7 +83,7 @@ public class KeyItemFix : Puzzle
 
             _currentItem = Inventory.Singleton.GetCurrentActiveItem();
 
-            _ProgressionAction?.Invoke(_ProgressionTimer / _ProgressionDuration);
+            ProgressionAction?.Invoke(_ProgressionTimer / _ProgressionDuration);
             yield return null;
         }
 
